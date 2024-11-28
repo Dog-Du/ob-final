2a44a5cdd502c6bc260adb5f5ba67063fc354d35
spiceandwolf <fanboyangchina@gmail.com>
2024-11-28 22:48:22 +0800

    faiss test

---
+++ deps/oblib/src/lib/vector/faiss/instructions_test_sse/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_64247.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_64247.dir/instructions_test_sse.cpp.o"
+  "cmTC_64247"
+  "cmTC_64247.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmTC_64247.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
