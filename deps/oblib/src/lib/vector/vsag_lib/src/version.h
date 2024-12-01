5203b13306a0bbb44a5ca6b5d7fe2b804d8c15bc
spiceandwolf <fanboyangchina@gmail.com>
2024-12-01 08:45:47 +0800

    rebase

--- deps/oblib/src/lib/vector/vsag_lib/src/version.h
+++ deps/oblib/src/lib/vector/vsag_lib/src/version.h
@@ -1,4 +1,4 @@
 #ifndef VSAG_VERSION_H_
 #define VSAG_VERSION_H_
-#define VSAG_VERSION "d7a6d99-dirty"
+#define VSAG_VERSION "5dec95e-dirty"
 #endif  /* VSAG_VERSION_H_ */
--- remake_ob.sh
+++ remake_ob.sh
@@ -1,6 +1,9 @@
 rm -rf ./tools/deploy/.obd/repository/oceanbase-ce/4.3.3.1
-./tools/deploy/obd.sh destroy -n obcluster --rm
+./tools/deploy/obd.sh destroy -n obcluster --rm < ./destroy.args
 echo "remove respository"
 bash ./build.sh release --init --make -j24
 ./tools/deploy/obd.sh prepare -p /tmp/obtest
-echo "4" | ./tools/deploy/obd.sh deploy -c obcluster.yaml
+./tools/deploy/obd.sh deploy -c obcluster.yaml < ./deploy.args
+bash ./link_ob.sh < ./create_ten.args
+bash ./load.sh
+bash ./run_ann_benchmarks.sh
